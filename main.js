2557fbe94a3269f5ef648ea26e6be519059bfe27
rodrigue malemba <rmalemba28@gmail.com>
2025-07-14 18:35:35 +0100

    update

--- main.js
+++ main.js
@@ -3,13 +3,19 @@ let port = document.getElementById("image1");
 
 let showPortfolio = () => {
     port.style.display = "block";
+    port.style.marginTop = "20px";
+    port.style.marginBottom = "10px";
 }
 readme.addEventListener("click", showPortfolio);
+
 
 const personnalP = document.getElementById("projetP");
 const open = document.getElementById("image2");
 
 const openPortfolio = () => {
     open.style.display = "block";
+    open.style.marginTop = "20px";
+    open.style.marginBottom = "10px";
 }
 personnalP.addEventListener("click", openPortfolio);
+
