18c1465902b223879c9dd85d0352342845ae3e5d
rodrigue malemba <rmalemba28@gmail.com>
2025-07-15 18:56:32 +0100

    update

--- main.js
+++ main.js
@@ -4,42 +4,42 @@ let image1 = document.getElementById("image1");
 
 let showImage1 = () => {
     image1.style.display = "block";
-    image1.style.marginTop = "20px";
-    image1.style.marginBottom = "10px";
+        image1.style.marginTop = "20px";
+            image1.style.marginBottom = "10px";
+                closeIcon1.style.display = "block";
 }
-portfolio.addEventListener("mouseover", showImage1);
-
+portfolio.addEventListener("click", showImage1);
+//masquer l'image1 au click
+const closeIcon1 = document.getElementById("close-icon1");
+const closeImage1 = () => {
+    image1.style.display = "none";
+    closeIcon1.style.display = "none";
+}
+closeIcon1.addEventListener("click", closeImage1)
 //affichage de la deuxieme image
 const personnalP = document.getElementById("projetP");
 const image2 = document.getElementById("image2");
 
 const showImage2 = () => {
     image2.style.display = "block";
-    image2.style.marginTop = "20px";
-    image2.style.marginBottom = "10px";
+        closeIcon.style.display = "block";
+            image2.style.marginTop = "20px";
+                image2.style.marginBottom = "10px";
 }
-personnalP.addEventListener("mouseover", showImage2);
+personnalP.addEventListener("click", showImage2);
+//masquer l'image2 au click
+const closeIcon = document.getElementById("close-icon");
+const closeImage2 = () => {
+    image2.style.display = "none";
+        closeIcon.style.display = "none";
+}
+closeIcon.addEventListener("click", closeImage2);
 //affichage de la troisieme image
 let projetAp = document.getElementById("projet-Ap");
 let image3 = document.getElementById("image3");
 
 const showImage3 = () => {
     image3.style.display = "block";
-    image3.style.marginTop = "20px";
+        image3.style.marginTop = "20px";
 }
-projetAp.addEventListener("mouseover", showImage3);
-//masquer l'image1
-const nextImage1 = document.getElementById("image1");
-const hideImage1 = () =>{
-    image1.style.display = "none";
-}
-nextImage1.addEventListener("click", hideImage1);
-
-//masquer l'image 2
-const nextImage2 = document.getElementById("image2");
-const hideImage2 = () =>{
-    image2.style.display = "none";
-    image2.style.transition = "2s";
-}
-nextImage2.addEventListener("click", hideImage2);
-
+projetAp.addEventListener("click", showImage3);
