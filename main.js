52e83eb883cfa85578b42d2b52fdd7eeb754db7a
rodrigue malemba <rmalemba28@gmail.com>
2025-07-20 10:52:54 +0100

    update

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
-//activer mode sombre
+//variable
 let darkMode = document.getElementById("dark-mode");
 let lightMode = document.getElementById("light-mode");
 let paragraph = document.querySelector("p");
 let corps = document.querySelector("body");
-
+let projetAp = document.getElementById("projet-Ap");
+let image3 = document.getElementById("image3");
+let portfolio = document.getElementById("portfolio");
+let image1 = document.getElementById("image1");
+const closeIcon1 = document.getElementById("close-icon1");
+const closeIcon = document.getElementById("close-icon");
+const personnalP = document.getElementById("projetP");
+const image2 = document.getElementById("image2");
+//activer mode sombre
 const activeDark = () => {
     corps.style.backgroundColor = "black";
     paragraph.style.color = "white";
     lightMode.style.backgroundColor = "white";
     lightMode.style.display = "block";
+    lightMode.style.borderRadius = "5px";
     darkMode.style.display = "none";
-
+    closeIcon1.style.backgroundColor = "white";
+    closeIcon.style.backgroundColor = "white";
+    closeIcon.style.borderRadius = "5px";
+    closeIcon1.style.borderRadius = "5px";
+    projetAp.style.color = "white";
+    portfolio.style.color = "white";
+    personnalP.style.color = "white";
 }
 darkMode.addEventListener("click", activeDark);
 
@@ -19,13 +34,16 @@ darkMode.addEventListener("click", activeDark);
 const activeLight = () => {
     paragraph.style.color = "black";
     darkMode.style.display = "block";
+    darkMode.style.borderRadius = "5px";
+    darkMode.style.border = "1px solid black"
     lightMode.style.display = "none";
     corps.style.backgroundColor = "white";
+    projetAp.style.color = "black";
+    portfolio.style.color = "black";
+    personnalP.style.color = "black";
 }
 lightMode.addEventListener("click", activeLight);
 //affichage de la premiere image
-let portfolio = document.getElementById("portfolio");
-let image1 = document.getElementById("image1");
 
 let showImage1 = () => {
     image1.style.display = "block";
@@ -35,15 +53,12 @@ let showImage1 = () => {
 }
 portfolio.addEventListener("click", showImage1);
 //masquer l'image1 au click
-const closeIcon1 = document.getElementById("close-icon1");
 const closeImage1 = () => {
     image1.style.display = "none";
     closeIcon1.style.display = "none";
 }
 closeIcon1.addEventListener("click", closeImage1)
 //affichage de la deuxieme image
-const personnalP = document.getElementById("projetP");
-const image2 = document.getElementById("image2");
 
 const showImage2 = () => {
     image2.style.display = "block";
@@ -53,15 +68,12 @@ const showImage2 = () => {
 }
 personnalP.addEventListener("click", showImage2);
 //masquer l'image2 au click
-const closeIcon = document.getElementById("close-icon");
 const closeImage2 = () => {
     image2.style.display = "none";
         closeIcon.style.display = "none";
 }
 closeIcon.addEventListener("click", closeImage2);
 //affichage de la troisieme image
-let projetAp = document.getElementById("projet-Ap");
-let image3 = document.getElementById("image3");
 
 const showImage3 = () => {
     image3.style.display = "block";
